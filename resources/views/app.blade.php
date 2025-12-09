8b35f55d6786751830c6b5d8e573c639781a070c
Jorge Silva <jorge.silva-10@outlook.com>
2025-12-09 17:52:03 -0600

    Add Favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="icon" type="image/jpeg" href="/images/logo.jpg">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
